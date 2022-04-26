0b72978fb624ba8e789f334b6aec3650046690a2
神明 <shenming@shenmingMac.local>
2022-04-26 10:42:24 +0800

    环境变量修改

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         proxy('/api', {
-            target: 'http://207.148.75.139:1000/api',
+            target: 'http://61.capital/api',
             changeOrigin: true,
             logLevel: 'debug',
             pathRewrite: {
