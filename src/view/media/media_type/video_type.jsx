4aa667bdd57e4ff992f83b5bb8687a1f710b5f14
神明 <shenming@shenmingMac.local>
2022-04-27 15:06:12 +0800

    环境变量补充

--- src/view/media/media_type/video_type.jsx
+++ src/view/media/media_type/video_type.jsx
@@ -1,6 +1,5 @@
 import "./_type.scss";
 import IconFont from "../../../comsmine/icon_font";
-import VideoImg from "../../../assets/images/video.png";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { message } from "antd";
